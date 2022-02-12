c32f800cf17967180e3a91121346814c2ece9a2f
Erin <erin.braz@gmail.com>
2022-02-12 12:34:03 -0500

    adding links to projects

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ gsap.from('.logo-sub', { duration: 1, y: '100%', ease: 'slow', delay: 0.2 })
 gsap.from('.logo-sub', { duration: 1, opacity: 0, ease: 'slow', delay: 0.2 })
 
 gsap.from('.nav-link', { duration: 0.5, opacity: 0, ease: 'slow', stagger: .5 })
+
+const project1 = document.getElementsByClassName('project1');
+const project2 = document.getElementsByClassName('project2');
+const project3 = document.getElementsByClassName('project3');
+const project4 = document.getElementsByClassName('project4');
+
+const project1URL = "https://meet-willow.netlify.app/"
+const project2URL = "https://whats-in-your-fridge-eb-af.netlify.app"
+const project3URL = "https://movie-finder-eb.netlify.app"
+const project4URL = "https://bingefest.netlify.app/"
+
+const windowFeatures = "location=yes,height=1080,width=1920,scrollbars=yes,status=yes";
+
+const onProjectClick = (project, projectURL) => {
+    project[0].addEventListener('click', function() {
+        window.open(projectURL, "_blank", windowFeatures);
+    })
+};
+
+// ur cool n fun 
+
+onProjectClick(project1, project1URL);
+onProjectClick(project2, project2URL);
+onProjectClick(project3, project3URL);
+onProjectClick(project4, project4URL);
